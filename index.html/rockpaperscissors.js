c3370977ec731eba39e1a0db5ba52639e85d3c05
Thomas Tang <thtang57@gmail.com>
2022-07-13 11:29:51 -0400

    Link to external JS

---
+++ index.html/rockpaperscissors.js
@@ -0,0 +1,8 @@
+        function computerPlay (a,b,c) {
+            return a || b || c 
+        }
+
+        a === ("Rock")
+        b === ("Paper")
+        c === ("Scissors")
+console.log(computerPlay);
