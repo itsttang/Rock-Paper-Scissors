8bed890c6159fbb26db088ae4ce2171b02039aea
Thomas Tang <thtang57@gmail.com>
2022-07-14 12:03:30 -0400

    Fix output of computer selection to Rock, Paper, or Scissors

--- index.html/rockpaperscissors.js
+++ index.html/rockpaperscissors.js
@@ -1,10 +1,7 @@
 
-
         function computerPlay (a,b,c) {
-            let options = ("Rock", "Paper", "Scissors");
-            return options = [Math.floor(Math.random()*3)];
+            let options = ["Rock", "Paper", "Scissors"];
+            return options [Math.floor(Math.random()*3)];
 
             }
-            computerPlay('Rock','Paper','Scissors');
-
-            console.log(computerPlay("Rock","Paper","Scissors"));
+            
