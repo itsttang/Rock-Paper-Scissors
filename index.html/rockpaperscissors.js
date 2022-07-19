befa2fdc3549e77a094e0be3155258ae9e5f286a
Thomas Tang <thtang57@gmail.com>
2022-07-19 10:41:10 -0400

    Restructure code and make all user input case-insensitive

--- index.html/rockpaperscissors.js
+++ index.html/rockpaperscissors.js
@@ -1,7 +1,40 @@
+const choices = ["rock", "paper", "scissors"];
 
-        function computerPlay (a,b,c) {
-            let options = ["Rock", "Paper", "Scissors"];
-            return options [Math.floor(Math.random()*3)];
+function game() { 
+    playRound();
+}
+             
+        // A round of rock, paper, scissors between the player and computer.
 
-            }
-            
+    function playRound() {
+        const playerSelection = playerChoice();
+        const computerSelection = computerChoice();
+        }
+        
+    function playerChoice() {
+        let input = prompt("Type Rock, Paper, or Scissors");
+        while (input == null) {
+            input = prompt("Type Rock, Paper, or Scissors");
+        }
+        
+        input = input.toLowerCase();
+        let check = validateInput(input)
+        if (check == true) {
+            console.log(input);
+        }
+        //console.log(input);
+    }
+        
+    function computerChoice() {
+        return choices[Math.floor(Math.random()*choices.length)];
+    }
+
+    function validateInput(choice) {
+        if (choices.includes(choice)) {
+            return true;
+        } else {
+            return false;
+        }
+        }
+    
+game();
